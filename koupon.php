5a418690c920d0ae7910916edefb05036cd1e05c
Sabbir Hasan <sabbirshouvo@gmail.com>
2021-05-11 01:24:51 +0600

    plugin file structure

---
+++ koupon.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * @wordpress-plugin
+ * Plugin Name:       Koupon
+ * Plugin URI:        http://example.com
+ * Description:       Intelligently Handle Koupon
+ * Version:           1.0.0
+ * Author:            Sabbir Hasan
+ * Author URI:        http://example.com/
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain:       koupon
+ * Domain Path:       /languages
+ */
